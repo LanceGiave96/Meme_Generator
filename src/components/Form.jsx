5d2240678603a2d0d7f476f14f2cd80b094d7223
LanceGiave96 <enricopedemonti@hotmail.it>
2024-01-28 16:32:40 +0100

    Add possibility to re-write text meme in two separate sections (top and bottomo)

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,31 @@
 import React from "react"
-import MemesObject from "/public/memesData.js"
 
 export default function Form(){
 
-    const [allMemeImages,setMemeImages] = React.useState(MemesObject)
+    const [memesArray,setMemeImages] = React.useState({})
+
+    //Use effect with .then() operations (no asyncronous)
+    /*
+    React.useEffect(() => { 
+        fetch("https://api.imgflip.com/get_memes")
+            .then( res => res.json() )
+                .then( data => {
+                        setMemeImages(data.data.memes)
+                    }
+                )
+    },[])
+    */
+
+    //Use effect using asincronous function (witnout .then())
+    React.useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setMemeImages(data.data.memes)
+        }
+        getMemes()
+    }, [])
+
     const [meme,setMeme] = React.useState(
         {
             topText: "",
@@ -11,10 +33,9 @@ export default function Form(){
             randomImage: ""
         }
     )
-
+    
     const getRandomMeme = function(){
         
-        let memesArray = allMemeImages.data.memes
         let random = Math.floor( Math.random() * memesArray.length )
         let url = memesArray[random].url
         setMeme( prevItem => (
@@ -27,6 +48,19 @@ export default function Form(){
     
     }
     
+    function handleText(event){
+        const {name,value} = event.target
+        setMeme( prevItem => (
+
+                {
+                    ...prevItem,
+                    [name]: value
+                }
+
+            )
+        )
+    }
+    
     return(
 
         <main>
@@ -37,11 +71,17 @@ export default function Form(){
                     type="text"
                     placeholder="Shut up"
                     className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleText}
                 />
                 <input 
                     type="text"
                     placeholder="And take my money"
                     className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleText}
                 />
                 <button 
                     className="form--button"
@@ -56,6 +96,8 @@ export default function Form(){
                 meme.randomImage && (
                                         <div className="meme--container" >
                                             <img src={meme.randomImage} className="image--meme" />
+                                            <h2 className="meme--text top">{meme.topText}</h2>
+                                            <h2 className="meme--text bottom">{meme.bottomText}</h2>
                                         </div>
                                     ) 
                 
